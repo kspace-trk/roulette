c44dac11488be16a77d8a4f3f28b886f0527f388
kspace-trk <keigonakao.0825@gmail.com>
2025-07-04 18:50:27 +0900

    chore: favicon設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
         }
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
       ]
     }
   },
